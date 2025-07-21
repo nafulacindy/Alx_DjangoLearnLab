f14f95ff60161178f52879c19d996b4f25f43ee5
nafulacindy <cindytalia022@gmail.com>
2025-07-21 22:57:57 +0300

    Implement advanced model relationships: Author, Book, Library, Librarian

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
 from django.views.generic import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .models import Library
 
 
 
