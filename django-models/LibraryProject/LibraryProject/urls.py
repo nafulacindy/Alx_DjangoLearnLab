f5bfcedf066bb9471ffdc4646d6d8c240f5c5529
nafulacindy <cindytalia022@gmail.com>
2025-07-21 21:20:30 +0300

    Implement advanced model relationships: Author, Book, Library, Librarian

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('home.urls')),  # now handles the root URL
     path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls'))
 ]
