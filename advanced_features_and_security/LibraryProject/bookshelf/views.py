ee340a1f415a768262ff22db4263f1793bdf5ffc
nafulacindy <cindytalia022@gmail.com>
2025-07-26 22:52:28 +0300

    🔐 Added permission-based access and security best practices

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.decorators import permission_required
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book
+from .forms import BookForm
+
+def create_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Make sure this matches your URL name
+    else:
+        form = BookForm()
+    
+    return render(request, 'bookshelf/create_book.html', {'form': form})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
