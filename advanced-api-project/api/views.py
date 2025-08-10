7dc6d1ade7ce2fb940188b0a5fc0c6429bdcb2e5
nafulacindy <cindytalia022@gmail.com>
2025-08-10 12:25:14 +0300

    Add generic views, permissions, and CRUD functionality for Book model

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -6,6 +6,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 
 
