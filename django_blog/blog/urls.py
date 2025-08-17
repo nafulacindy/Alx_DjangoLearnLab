b8fb5bfe5f18a05027aba1b522d62896b90e404d
nafulacindy <cindytalia022@gmail.com>
2025-08-17 23:41:41 +0300

    Initial Django blog project commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+)
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -11,4 +18,9 @@ urlpatterns = [
     path("posts/", views.post_list, name="post_list"),
     path("posts/<int:pk>/", views.post_detail, name="post_detail"),
     path("search/", views.search, name="search"),
+    path("", PostListView.as_view(), name="post_list"),   # homepage -> list of posts
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),  # view post
+    path("post/new/", PostCreateView.as_view(), name="post_new"),  # create new post
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),  # edit
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),  # delete
 ]
